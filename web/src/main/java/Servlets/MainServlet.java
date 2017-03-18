151a4a6587dea982fa7d92a53cdded8f35e39d5f
Andrey <Boykooo@bk.ru>
2017-03-19 01:45:46 +0300

    Changed startPage.jsp

--- web/src/main/java/Servlets/MainServlet.java
+++ web/src/main/java/Servlets/MainServlet.java
@@ -1,5 +1,4 @@
-import Entities.User;
-import services.UserService;
+package Servlets;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,15 +10,6 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        UserService service = new UserService();
-
-        User user = service.findById(18);
-
-        if (user != null) {
-            req.setAttribute("name", user.getFirstName());
-        }
-
         req.getRequestDispatcher("startPage.jsp").forward(req, resp);
     }
 }
---
+++ web/src/main/webapp/pages/usersPage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Andrey
+  Date: 19.03.2017
+  Time: 1:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Users</title>
+</head>
+<body>
+<h2>This page is for displaying users</h2>
+</body>
+</html>

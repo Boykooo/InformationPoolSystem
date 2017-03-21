361e66d1fc89db0a4093d97fd7f4ee467d688981
Andrey <Boykooo@bk.ru>
2017-03-21 19:46:32 +0300

    Imlp @Get specific user

--- web/src/main/java/rest/UserRest.java
+++ web/src/main/java/rest/UserRest.java
@@ -6,19 +6,34 @@ import services.UserService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/users")
-@Produces("text/json")
 public class UserRest {
 
-    @GET
-    public List<User> getAllUsers(){
-        UserService userService = new UserService();
-        List<User> userList = userService.findAll();
+    UserService service;
 
-        return userList;
+    public UserRest() {
+        service = new UserService();
+    }
+
+    @GET
+    @Produces("text/json")
+    public List<User> getAllUsers(){
+        return service.findAll();
+    }
+
+    @GET
+    @Produces("text/json")
+    @Path("/{userId}")
+    public Response getSpecificUsers(@PathParam("userId") int userId){
+        User user = service.findById(userId);
+
+        return (user == null) ? Response.status(Response.Status.NOT_FOUND).build()
+                              : Response.ok(user).build();
     }
 
     @GET
